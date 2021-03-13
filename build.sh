9de1dcee9aee4b21810f31a91d38fe48ea9abb34
stdpain <drfeng08@gmail.com>
2021-03-13 13:41:13 +0800

    [dependence] add thirdparty gperftools

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 export CC=/opt/compiler/gcc-10/bin/gcc
 export CXX=/opt/compiler/gcc-10/bin/g++
+export PATH=/opt/compiler/gcc-10/bin:$PATH
 export LIBRT=/opt/compiler/gcc-10/lib64/librt.so
 
 BUILD_THREAD=12
@@ -10,4 +11,12 @@ DIR=$(cd $(dirname $0) && pwd )
 
 #rm -rf build
 mkdir $DIR/$BUILD_DIR
-cd $DIR/$BUILD_DIR && cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DLIBRT=$LIBRT -DCMAKE_INSTALL_PREFIX=`pwd`/install ..&& make -j $BUILD_THREAD  && make install
+cd $DIR/$BUILD_DIR && 
+    cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+        -DCMAKE_BUILD_TYPE=DEBUG \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -Dgperftools_enable_libunwind=NO \
+        -Dgperftools_enable_frame_pointers=ON \
+        -DLIBRT=$LIBRT \
+        -DCMAKE_INSTALL_PREFIX=`pwd`/install .. \
+        && make -j $BUILD_THREAD  && make install
