1fc3f5879d86f5927f6228bf268b25ffdefda395
stdpain <drfeng08@gmail.com>
2022-10-14 16:12:46 +0800

    update

--- test/bench/bench_map.cpp
+++ test/bench/bench_map.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <random>
+#include <sparsehash/dense_hash_map>
+#include <sparsehash/sparse_hash_map>
 #include <unordered_map>
 
 constexpr int data_range = 5'000'000;
@@ -12,14 +14,14 @@ static std::default_random_engine e;
 static std::uniform_int_distribution<int> u(0, data_range);
 
 template <class MAP, bool prepare_work = true>
-void insert(benchmark::State& state) {
-    MAP raw_map;
+void insert(MAP& raw_map, benchmark::State& state) {
     std::vector<int64_t> input_column(chunk_size);
     // prepare work
     // prepare a large hash table
     if constexpr (prepare_work) {
         for (size_t i = 0; i < data_range * 2; ++i) {
-            raw_map.try_emplace(u(e), nullptr);
+            // raw_map.try_emplace(u(e), nullptr);
+            raw_map.insert({u(e), nullptr});
         }
     }
 
@@ -32,26 +34,38 @@ void insert(benchmark::State& state) {
         }
         state.ResumeTiming();
         for (size_t i = 0; i < chunk_size; ++i) {
-            raw_map.try_emplace(input_column[i], nullptr);
+            // raw_map.try_emplace(input_column[i], nullptr);
+            raw_map.insert({input_column[i], nullptr});
         }
     }
     std::cout << "map size:" << raw_map.size() << std::endl;
 }
 
 static void std_insert_test(benchmark::State& state) {
-    insert<std::unordered_map<int64_t, char*>>(state);
+    std::unordered_map<int64_t, char*> raw_map{};
+    insert(raw_map, state);
 }
 
 static void flat_hash_map_insert_test(benchmark::State& state) {
-    insert<phmap::flat_hash_map<int64_t, char*>>(state);
+    phmap::flat_hash_map<int64_t, char*> raw_map{};
+    insert(raw_map, state);
 }
 
 static void robin_hash_map_insert_test(benchmark::State& state) {
-    insert<tsl::robin_map<int64_t, char*>>(state);
+    tsl::robin_map<int64_t, char*> raw_map;
+    insert(raw_map, state);
+
+}
+
+static void dense_hash_map_insert_test(benchmark::State& state) {
+    google::dense_hash_map<int64_t, char*> raw_map;
+    raw_map.set_empty_key(0);
+    insert(raw_map, state);
 }
 
 BENCHMARK(std_insert_test);
 BENCHMARK(flat_hash_map_insert_test);
 BENCHMARK(robin_hash_map_insert_test);
+BENCHMARK(dense_hash_map_insert_test);
 
 BENCHMARK_MAIN();
