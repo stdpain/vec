c658a11ce6c3e7355cc1e5df600675e758b63a1d
stdpain <drfeng08@gmail.com>
2024-12-15 17:00:26 +0800

    update mem/SPSC benchmark

--- parallel/src/queue/bench_ring_buffer.cpp
+++ parallel/src/queue/bench_ring_buffer.cpp
@@ -4,21 +4,28 @@
 #include <thread>
 
 #include "ring_buffer.h"
-constexpr int64_t MAXN = int64_t(1e8);
-// benchmark on Intel(R) Xeon(R) Platinum 8269CY CPU @ 2.50GHz (104 CPUS)
-MutexedRingBuffer<spinlock> queue(1023); // 11.3099s 
-// MutexedRingBuffer<std::mutex> queue(1023); // 23.5687s
-// LockFreeRingBufferV1 queue(1023); // 2.24, 2.30
-// LockFreeRingBufferV2<1024> queue; // 1.50, 1.55
-// LockFreeRingBufferV3<1024, 60, 60> queue; // 1.02, 1.05
-// LockFreeRingBufferV3<1024, 60, 120> queue; // 0.82, 0.85
 
-int main() {
+constexpr int64_t MAXN = int64_t(1e8);
+
+struct ThreadBind {
+    static constexpr auto consumer_id = 1;
+    static constexpr auto producer_id = 2;
+};
+
+struct SingleThreadBind {
+    static constexpr auto consumer_id = 1;
+    static constexpr auto producer_id = 1;
+};
+
+// benchmark for SPSC
+template <class QueueType, class ThreadBindType = ThreadBind, class... Args>
+void bench_queue(const char* name, Args&&... args) {
+    QueueType queue(std::forward<Args>(args)...);
     auto start = std::chrono::steady_clock::now();
-    std::thread pop_thread([]() {
+    std::thread pop_thread([&queue]() {
         cpu_set_t cpuset;
         CPU_ZERO(&cpuset);
-        CPU_SET(1, &cpuset);
+        CPU_SET(ThreadBindType::consumer_id, &cpuset);
         pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
         volatile int64_t cnt = 0, last = 0;
         while (cnt != MAXN) {
@@ -33,10 +40,10 @@ int main() {
             }
         }
     });
-    std::thread push_thread([]() {
+    std::thread push_thread([&queue]() {
         cpu_set_t cpuset;
         CPU_ZERO(&cpuset);
-        CPU_SET(2, &cpuset);
+        CPU_SET(ThreadBindType::producer_id, &cpuset);
         pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
         for (int64_t i = 1; i <= MAXN;) {
             if (queue.is_full()) {
@@ -51,6 +58,25 @@ int main() {
     pop_thread.join();
     auto finish = std::chrono::steady_clock::now();
     auto runtime = std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count();
-    std::cerr << runtime / 1e6 << "s" << std::endl;
+    std::cerr << name << " costs:" << runtime / 1e6 << "s" << std::endl;
+}
+
+int main() {
+    // benchmark on Intel(R) Xeon(R) Platinum 8269CY CPU @ 2.50GHz (104 CPUS)
+    // 11.3099s
+    bench_queue<MutexedRingBuffer<spinlock>>("spin lock buffer", 1023);
+    // 23.5687s
+    bench_queue<MutexedRingBuffer<std::mutex>>("std::mutex lock buffer", 1023);
+    // 2.24, 2.30
+    bench_queue<LockFreeRingBufferV1>("LockFreeRingBufferV1", 1023);
+    // 1.50, 1.55
+    bench_queue<LockFreeRingBufferV2<1024>>("LockFreeRingBufferV2");
+    // 1.02, 1.05
+    bench_queue<LockFreeRingBufferV3<1024, 60, 60>>("LockFreeRingBufferV3");
+    // 0.82, 0.85
+    bench_queue<LockFreeRingBufferV3<1024, 60, 120>>("LockFreeRingBufferV4");
+    // 0.815
+    bench_queue<LockFreeRingBufferV3<1024, 60, 120>, SingleThreadBind>("single thread");
+
     return 0;
 }
--- test/bench/bench_mem_access.cpp
+++ test/bench/bench_mem_access.cpp
@@ -1,89 +1,145 @@
+// https://stackoverflow.com/questions/40950254/speed-up-random-memory-access-using-prefetch
+
 #include <benchmark/benchmark.h>
+#include <emmintrin.h>
 #include <limits.h>
 
-// constexpr int BUFFER_SIZE = 4096 * 100000;
-constexpr int BUFFER_SIZE = 4096 * 250;
+#include <cstdlib>
+
 constexpr int chunk_size = 4096;
-unsigned int* valueBuffer;
-unsigned int* indexBuffer;
 
-unsigned int randomUint() {
-    int value = rand() % UINT_MAX;
-    return value;
-}
+struct BenchContext {
+    size_t buffer_size = 0;
+    unsigned int* valueBuffer = nullptr;
+    unsigned int* indexBuffer = nullptr;
+    template <void (BenchContext::*index_creator)(int)>
+    void init(size_t buffer_size) {
+        this->buffer_size = buffer_size;
+        valueBuffer = (unsigned int*)malloc(buffer_size * sizeof(unsigned int));
+        indexBuffer = (unsigned int*)malloc(buffer_size * sizeof(unsigned int));
+        createValueBuffer(buffer_size);
+        (this->*index_creator)(buffer_size);
+    }
+    ~BenchContext() { close(); }
 
-unsigned int* createValueBuffer() {
-    for (unsigned long i = 0; i < BUFFER_SIZE; i++) {
-        valueBuffer[i] = randomUint();
+    unsigned int randomUint() {
+        int value = rand() % UINT_MAX;
+        return value;
     }
 
-    return (valueBuffer);
-}
+    unsigned int* createValueBuffer(int buffer_size) {
+        for (unsigned long i = 0; i < buffer_size; i++) {
+            valueBuffer[i] = randomUint();
+        }
 
-unsigned int* createRandIndexBuffer() {
-    for (unsigned long i = 0; i < BUFFER_SIZE; i++) {
-        indexBuffer[i] = rand() % BUFFER_SIZE;
+        return (valueBuffer);
     }
 
-    return (indexBuffer);
-}
-
-unsigned int* createLinerIndexBuffer() {
-    for (unsigned long i = 0; i < BUFFER_SIZE; i++) {
-        indexBuffer[i] = i;
+    void createLinerIndexBuffer(int buffer_size) {
+        for (unsigned long i = 0; i < buffer_size; i++) {
+            indexBuffer[i] = i;
+        }
     }
 
-    return (indexBuffer);
-}
+    void createRandIndexBuffer(int buffer_size) {
+        for (unsigned long i = 0; i < buffer_size; i++) {
+            indexBuffer[i] = rand() % buffer_size;
+        }
+    }
 
-template <unsigned int* index_creator()>
-void init() {
-    valueBuffer = (unsigned int*)malloc(BUFFER_SIZE * sizeof(unsigned int));
-    indexBuffer = (unsigned int*)malloc(BUFFER_SIZE * sizeof(unsigned int));
-    index_creator();
-}
+    void close() {
+        free(valueBuffer);
+        free(indexBuffer);
+    }
+};
 
-void close() {
-    free(valueBuffer);
-    free(indexBuffer);
-}
+struct Noop {
+    void operator()(BenchContext& context, size_t offset) {}
+};
 
-__attribute_noinline__ void call_sum(int start, int end, int64_t& sum) {
+struct FlushCache {
+    void operator()(BenchContext& context, size_t offset) {
+        for (size_t i = offset; i < chunk_size; ++i) {
+            unsigned int index = context.indexBuffer[i];
+            _mm_clflush(context.indexBuffer + index);
+            _mm_clflush(context.valueBuffer + index);
+        }
+    }
+};
+
+__attribute_noinline__ void call_sum(unsigned int* __restrict valueBuffer,
+                                     unsigned int* __restrict indexBuffer, int start, int end,
+                                     int64_t& sum) {
     for (unsigned int i = start; i < end; i++) {
         unsigned int index = indexBuffer[i];
         sum += valueBuffer[index];
     }
 }
 
-int next(int id) {
-    if (id + chunk_size < BUFFER_SIZE) {
+int next(int id, size_t buffer_size) {
+    if (id + chunk_size < buffer_size) {
         return id + chunk_size;
     }
     return 0;
 }
 
+template <size_t buffer_size, class Pre = Noop>
 void mem_liner_access(benchmark::State& state) {
-    init<createLinerIndexBuffer>();
+    srand(0);
+    BenchContext context;
+    context.init<&BenchContext::createLinerIndexBuffer>(buffer_size);
     int64_t sum = 0;
     int id = 0;
     for (auto _ : state) {
-        call_sum(id, id + chunk_size, sum);
-        id = next(id);
+        state.PauseTiming();
+        Pre()(context, id);
+        state.ResumeTiming();
+        call_sum(context.valueBuffer, context.indexBuffer, id, id + chunk_size, sum);
+        id = next(id, buffer_size);
     }
-    close();
 }
 
+template <size_t buffer_size, class Pre = Noop>
 void run_rand_access(benchmark::State& state) {
-    init<createRandIndexBuffer>();
+    srand(0);
+    BenchContext context;
+    context.init<&BenchContext::createRandIndexBuffer>(buffer_size);
     int64_t sum = 0;
     int id = 0;
     for (auto _ : state) {
-        call_sum(id, id + chunk_size, sum);
-        id = next(id);
+        state.PauseTiming();
+        Pre()(context, id);
+        state.ResumeTiming();
+        call_sum(context.valueBuffer, context.indexBuffer, id, id + chunk_size, sum);
+        id = next(id, buffer_size);
     }
-    close();
 }
-BENCHMARK(run_rand_access);
-BENCHMARK(mem_liner_access);
+
+BENCHMARK_TEMPLATE(mem_liner_access, 4096 * 10);
+BENCHMARK_TEMPLATE(mem_liner_access, 4096 * 250);
+BENCHMARK_TEMPLATE(mem_liner_access, 4096 * 1000);
+BENCHMARK_TEMPLATE(run_rand_access, 4096 * 10);
+BENCHMARK_TEMPLATE(run_rand_access, 4096 * 10, FlushCache);
+BENCHMARK_TEMPLATE(run_rand_access, 4096 * 250);
+BENCHMARK_TEMPLATE(run_rand_access, 4096 * 1000);
+// slow benchmarks...
+BENCHMARK_TEMPLATE(mem_liner_access, 4096 * 100000);
+BENCHMARK_TEMPLATE(run_rand_access, 4096 * 100000);
 
 BENCHMARK_MAIN();
+
+// benchmark on Intel(R) Xeon(R) Platinum 8269CY CPU @ 2.50GHz (104 CPUS)
+// CPU Caches:
+//   L1 Data 32 KiB (x52)
+//   L1 Instruction 32 KiB (x52)
+//   L2 Unified 1024 KiB (x52)
+//   L3 Unified 36608 KiB (x2)
+// mem_liner_access<4096 * 10>                  2474 ns         2490 ns       281048
+// mem_liner_access<4096 * 250>                 2521 ns         2531 ns       275732
+// mem_liner_access<4096 * 1000>                2745 ns         2756 ns       247025
+// run_rand_access<4096 * 10>                   2497 ns         2511 ns       278192
+// run_rand_access<4096 * 10, FlushCache>       5549 ns         5096 ns       137866
+// run_rand_access<4096 * 250>                  9067 ns         9086 ns        76910
+// run_rand_access<4096 * 1000>                14822 ns        14865 ns        38327
+// mem_liner_access<4096 * 100000>              3528 ns         3547 ns       203628
+// run_rand_access<4096 * 100000>              59816 ns        59893 ns        11171
