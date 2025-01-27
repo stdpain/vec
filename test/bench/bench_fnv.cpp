a9402e26bc12f4e47d002a9bc8c0059bc0ae4135
stdpain <drfeng08@gmail.com>
2025-01-27 15:08:40 +0800

    update benchmark for fnv hash

--- test/bench/bench_fnv.cpp
+++ test/bench/bench_fnv.cpp
@@ -10,19 +10,21 @@
 #include <vector>
 
 static const uint32_t FNV_PRIME = 0x01000193; //   16777619
+static const uint32_t FNV_SEED = 0x811c9dc5;
 
-uint32_t fnv_hash(const int32_t* data, int32_t bytes, uint32_t hash) {
-    const uint8_t* ptr = reinterpret_cast<const uint8_t*>(data);
+inline uint32_t fnv_1a_hash(const int32_t* data, int32_t bytes, uint32_t hash) {
+    unsigned char* bp = (unsigned char*)data; /* start of buffer */
+    unsigned char* be = bp + bytes;           /* beyond end of buffer */
 
-    while (bytes--) {
-        hash = (*ptr ^ hash) * FNV_PRIME;
-        ++ptr;
+    while (bp < be) {
+        hash ^= *bp++;
+        hash *= FNV_PRIME;
     }
 
     return hash;
 }
 
-uint32_t fnv_1a_hash(const int32_t* data, int32_t bytes, uint32_t hash) {
+inline uint32_t fnv_1_hash(const int32_t* data, int32_t bytes, uint32_t hash) {
     unsigned char* bp = (unsigned char*)data; /* start of buffer */
     unsigned char* be = bp + bytes;           /* beyond end of buffer */
 
@@ -36,19 +38,19 @@ uint32_t fnv_1a_hash(const int32_t* data, int32_t bytes, uint32_t hash) {
     return hash;
 }
 
-void batch_call_fnv_hash(int* data, int hash, int size) {
-    for (int i = 0; i < size; ++i) {
-        data[i] = fnv_hash(&data[i], 4, hash);
-    }
-}
-
 void batch_call_fnv_1a_hash(int* data, int hash, int size) {
     for (int i = 0; i < size; ++i) {
         data[i] = fnv_1a_hash(&data[i], 4, hash);
     }
 }
 
-void simd_batch_call_fnv_hash(int* data, int hash_base, int size) {
+void batch_call_fnv_1_hash(int* data, int hash, int size) {
+    for (int i = 0; i < size; ++i) {
+        data[i] = fnv_1_hash(&data[i], 4, hash);
+    }
+}
+
+void simd_batch_call_fnv_1a_hash(int* data, int hash_base, int size) {
     __m256i fnv_prime_avx = _mm256_set1_epi32(FNV_PRIME);
     __m256i mask = _mm256_set1_epi32(0xFF);
     for (int i = 0; i < size / 8; ++i) {
@@ -66,10 +68,15 @@ void simd_batch_call_fnv_hash(int* data, int hash_base, int size) {
 
 void simd_batch_call_fnv_1a_32_hash(int* data, int hash_base, int size) {
     __m256i fnv_prime_avx = _mm256_set1_epi32(FNV_PRIME);
+    __m256i mask = _mm256_set1_epi32(0xFF);
     for (int i = 0; i < size / 8; ++i) {
         __m256i load = _mm256_loadu_si256((__m256i*)data);
         __m256i hash = _mm256_set1_epi32(hash_base);
-        hash = _mm256_mullo_epi32(_mm256_xor_si256(load, hash), fnv_prime_avx);
+        for (int j = 0; j < 4; ++j) {
+            __m256i data = _mm256_and_si256(load, mask);
+            hash = _mm256_mullo_epi32(_mm256_xor_si256(data, hash), fnv_prime_avx);
+            load = _mm256_srli_epi32(load, 8);
+        }
         _mm256_storeu_si256(reinterpret_cast<__m256i*>(data), hash);
         data += 8;
     }
@@ -78,8 +85,8 @@ void simd_batch_call_fnv_1a_32_hash(int* data, int hash_base, int size) {
 void validate(std::vector<int>& data) {
     auto dup = data;
     auto dup2 = data;
-    batch_call_fnv_hash(dup.data(), 0, 4096);
-    simd_batch_call_fnv_hash(data.data(), 0, 4096);
+    batch_call_fnv_1a_hash(dup.data(), FNV_SEED, 4096);
+    simd_batch_call_fnv_1a_hash(data.data(), FNV_SEED, 4096);
     for (int i = 0; i < data.size(); ++i) {
         if (dup[i] != data[i]) {
             std::abort();
@@ -90,15 +97,14 @@ void validate(std::vector<int>& data) {
 void validate_fnv_1a_32(std::vector<int>& data) {
     auto dup = data;
     auto dup2 = data;
-    batch_call_fnv_1a_hash(dup.data(), 0, 4096);
-    simd_batch_call_fnv_1a_32_hash(data.data(), 0, 4096);
+    batch_call_fnv_1_hash(dup.data(), FNV_SEED, 4096);
+    simd_batch_call_fnv_1a_32_hash(data.data(), FNV_SEED, 4096);
     for (int i = 0; i < data.size(); ++i) {
         if (dup[i] != data[i]) {
             std::abort();
         }
     }
 }
-
 
 constexpr int elements_sz = 4096;
 
@@ -121,7 +127,7 @@ Context initCtx() {
 static void FNV_Impl(benchmark::State& state) {
     auto ctx = initCtx();
     for (auto _ : state) {
-        batch_call_fnv_hash(ctx.input.data(), 0, 4096);
+        batch_call_fnv_1a_hash(ctx.input.data(), FNV_SEED, 4096);
         benchmark::DoNotOptimize(ctx);
     }
 }
@@ -129,7 +135,7 @@ static void FNV_Impl(benchmark::State& state) {
 static void FNV_1A_32_Impl(benchmark::State& state) {
     auto ctx = initCtx();
     for (auto _ : state) {
-        batch_call_fnv_1a_hash(ctx.input.data(), 0, 4096);
+        batch_call_fnv_1_hash(ctx.input.data(), FNV_SEED, 4096);
         benchmark::DoNotOptimize(ctx);
     }
 }
@@ -137,7 +143,7 @@ static void FNV_1A_32_Impl(benchmark::State& state) {
 static void FNV_1A_32_SIMD_Impl(benchmark::State& state) {
     auto ctx = initCtx();
     for (auto _ : state) {
-        simd_batch_call_fnv_1a_32_hash(ctx.input.data(), 0, 4096);
+        simd_batch_call_fnv_1a_32_hash(ctx.input.data(), FNV_SEED, 4096);
         benchmark::DoNotOptimize(ctx);
     }
 }
@@ -145,7 +151,7 @@ static void FNV_1A_32_SIMD_Impl(benchmark::State& state) {
 static void FNV_SIMDImpl(benchmark::State& state) {
     auto ctx = initCtx();
     for (auto _ : state) {
-        simd_batch_call_fnv_hash(ctx.input.data(), 0, 4096);
+        simd_batch_call_fnv_1a_hash(ctx.input.data(), FNV_SEED, 4096);
         benchmark::DoNotOptimize(ctx);
     }
 }
@@ -166,7 +172,6 @@ static void validate_fnv_1a_32(benchmark::State& state) {
     }
 }
 
-
 BENCHMARK(FNV_Impl);
 BENCHMARK(FNV_1A_32_Impl);
 BENCHMARK(FNV_SIMDImpl);
@@ -178,9 +183,9 @@ BENCHMARK_MAIN();
 // --------------------------------------------------------------
 // Benchmark                    Time             CPU   Iterations
 // --------------------------------------------------------------
-// FNV_Impl                 20510 ns        20510 ns        34126
-// FNV_1A_32_Impl           12378 ns        12377 ns        56066
-// FNV_SIMDImpl              2163 ns         2163 ns       333651
-// FNV_1A_32_SIMD_Impl        357 ns          357 ns      1846299
-// validate_fnv             27693 ns        27691 ns        25227
-// validate_fnv_1a_32       27621 ns        27619 ns        25316
+// FNV_Impl                  5144 ns         5144 ns       136199
+// FNV_1A_32_Impl            4513 ns         4512 ns       155254
+// FNV_SIMDImpl              2079 ns         2079 ns       336490
+// FNV_1A_32_SIMD_Impl       2063 ns         2063 ns       336803
+// validate_fnv             11638 ns        11637 ns        59869
+// validate_fnv_1a_32       11575 ns        11574 ns        60583
